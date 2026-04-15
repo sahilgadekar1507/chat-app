c6eca04f96ea9e290bff9b0ccacb2949c5cf60eb
sahilgadekar1507 <sahilgadekar15@gmail.com>
2026-04-15 10:41:03 +0530

    regex route added

--- backend/src/index.js
+++ backend/src/index.js
@@ -38,7 +38,7 @@ if (process.env.NODE_ENV === "production") {
   const frontendPath = path.join(__dirname, "frontend", "dist");
   app.use(express.static(frontendPath));
 
-  app.get("*", (req, res) => {
+  app.use(/.*/, (req, res) => {
     res.sendFile(path.join(frontendPath, "index.html"));
   });
 }
